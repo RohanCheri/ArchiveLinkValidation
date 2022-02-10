f3c2e51ab65ccbd42d71177953041c4d6c7232d0
RohanCheri <34838212+RohanCheri@users.noreply.github.com>
2022-02-10 15:46:53 -0800

    Push second version of command line argument parsing

--- src/main.py
+++ src/main.py
@@ -1,35 +1,38 @@
 import sys
 import constants
+import validate_archive
 
-archiveLink = ""
-outputDirectory = ""
-logging = False
+
+# Main program to process run arguments
 def main():
-    global outputDirectory, logging, archiveLink
+    try:
+        validateArchive = validate_archive.ValidateArchive()
 
-    args = sys.argv  # List of program arguments
-    i = 1
-    while i < len(args):
-        arg = args[i]
-        if arg == '-h' or arg == '--help':
-            print(constants.HELP_STRING)
-            return
-        elif arg == '-a' or arg == '--archive_link':
+        args = sys.argv  # List of program arguments
+        i = 1
+        while i < len(args):
+            arg = args[i]
+            if arg == '-h' or arg == '--help':
+                print(constants.HELP_STRING)
+                return
+            elif arg == '-a' or arg == '--archive_link':
+                i += 1
+                validateArchive.archiveLink = args[i]
+            elif arg == '-o' or arg == '--outdir':
+                i += 1
+                validateArchive.outputDirectory = args[i]
+            elif arg == '-l' or arg == '--log':
+                i += 1
+                validateArchive.logging = True
+
             i += 1
-            archiveLink = args[i]
-        elif arg == '-o' or arg == '--outdir':
-            i += 1
-            outputDirectory = args[i]
-        elif arg == '-l' or arg == '--log':
-            i += 1
-            logging = True
 
-        i += 1
-
-    print(archiveLink)
-    print(outputDirectory)
-    print(logging)
+        validateArchive.validateInformation()
+    except Exception as e:
+        print(e)
+        exit(1)
 
 
+# Call to main function
 if __name__ == "__main__":
     main()
